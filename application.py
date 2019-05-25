d2ca4c1e38c633c8537ff950126ca820d7a8a0e5
Fernando <lsmupiita@gmail.com>
2019-05-25 03:37:22 -0500

    Codigo

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import codigo
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 
@@ -12,12 +14,19 @@ class HelloWorld(Resource):
         some_json = request.get_json()
         return {'you sent': some_json}
 
+class Codigo(Resource):
+    def get(self,correo):
+        return {'codigo':codigo.generarCodigo(correo)}
+        #return {'you sent': num}
+
 class Multi(Resource):
     def get(self,num):
         return {'result': num*10}
 
 api.add_resource(HelloWorld,'/')
 api.add_resource(Multi,'/multi/<int:num>')
+
+api.add_resource(Codigo,'/codigo/<string:correo>')
 
 if __name__ == '__main__':
     app.run(debug=True)
